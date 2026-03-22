7ae75956a231eecbc11f8b65afd7ae0167b6d18d
Harshit Ahuja <harshit@Harshits-MacBook-Air.local>
2026-03-22 14:49:28 +0530

    update

--- Multithreading/ThreadsWithRunnable.java
+++ Multithreading/ThreadsWithRunnable.java
@@ -1,3 +1,11 @@
+/*
+'Runnable' is a functional interface in java used for multithreading.
+Functional Interface - Interface that contains just one abstract method but can contain multiple defaults or static methods.
+Also called 'SAM' Interface - Single Abstract Method Interface
+Runnable interface has one abstract method - 'run()' which needs to be overrided while implementing Runnable.
+'run()' method should define the actual logic that the thread needs to perform.
+*/
+
 class MyRunnableTask implements Runnable {
 
     @Override
